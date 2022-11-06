f6027f7ecbdbbd1ad56daf2e75c8c63d38fbed1c
HansKim <hanskim6@gmail.com>
2022-11-06 12:58:51 +0000

    Chapter 04-2

--- nomadcoder/movie_app_2020/src/App.js
+++ nomadcoder/movie_app_2020/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <img src={picture} alt=''></img>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    key: 1,
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+  },
+  {
+    key: 2,
+    name: 'Samgyeopsal',
+    image: 'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
+  },
+  {
+    key: 3,
+    name: 'Bibimbap',
+    image: 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
+  },
+  {
+    key: 4,
+    name: 'Kimbap',
+    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodILike.map(dish => (<Food key={dish.key} name={dish.name} picture={dish.image} />))}
+  </div>
   );
 }
 
